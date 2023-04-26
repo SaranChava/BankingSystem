d5faddecb9f511a5e2e223f9ad9023d122094bff
RahulRajKattela <rkattela@uncc.edu>
2023-04-26 15:40:10 -0400

    old commits

---
+++ Code/new_offers.php
@@ -0,0 +1,41 @@
+<html>
+<head><title>New Offers for Sales</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<body>
+
+<?php  
+
+	include 'Emp_profile_header.php' ;
+?>
+<div class="new_offers_container">
+
+<table border="1px" cellpadding="5">
+			   <th>Offer</th>
+			   <th>Validity</th>
+			   
+<?php
+
+			
+		echo '
+			<tr>
+			<td> "ABC Bank offers a high-yield online savings account with a 0.50% APY." </td>
+			<td>"05-May-2023"</td></tr>
+            <tr>
+			<td> "Get lowest interest rate on direct loan application from bank" </td>
+			<td>"10-May-2023"</td></tr>
+            <tr>
+            <td> "ABC offers the Cred® Cash Rewards credit card, which gives you 4% cash back on a category of your choice, 3% cash back at grocery stores and wholesale clubs, and 2% cash back on all other purchases." </td>
+			<td>"06-May-2023"</td>
+			</tr>';
+
+?>
+
+</table>
+</div>
+</body>
+</html>
+
+
+
+
