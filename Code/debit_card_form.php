14bdaf7725b613ddf5d0fdbbd8d6bdb8e2d4ad53
RahulRajKattela <rkattela@uncc.edu>
2023-04-26 15:25:02 -0400

    debit card form changes

--- Code/debit_card_form.php
+++ Code/debit_card_form.php
@@ -7,11 +7,11 @@
 <div class="debit_card_form_container">
     <br>
 <form method="POST">
-<input type="text" name="holder_name" placeholder="Account Holder Name"><br>
+<input type="text" name="holder_name" placeholder="Name of the account holder"><br>
 <input type="text" name="dob" placeholder="Date of Birth" onfocus="(this.type='date')"><br>
-<input type="text" name="PAN" placeholder="PAN"><br>
-<input type="text" name="mob" placeholder="Registered Mobile (10 Digit)"><br>
-<input type="text" name="acc_no" placeholder="Account No"><br>
+<input type="text" name="SSN" placeholder="SSN"><br>
+<input type="text" name="mob" placeholder="Mobile Number"><br>
+<input type="text" name="acc_no" placeholder="Account Number"><br>
 <input type="submit" name ="dbt_crd_submit" value"Submit" ><br>
 <form>
 </div>
@@ -24,12 +24,12 @@
 if(isset($_POST['dbt_crd_submit'])){
     $holder_name = $_POST['holder_name'];
     $dob = $_POST['dob'];
-    $PAN = $_POST['PAN'];
+    $SSN = $_POST['SSN'];
     $mob = $_POST['mob'];
     $acc_no = $_POST['acc_no'];
-    if(empty($_POST['holder_name']) || empty($_POST['dob']) || empty($_POST['PAN']) ||empty($_POST['mob']) ||empty($_POST['acc_no'])){
+    if(empty($_POST['holder_name']) || empty($_POST['dob']) || empty($_POST['SSN']) ||empty($_POST['mob']) ||empty($_POST['acc_no'])){
 
-        echo '<script>alert("No field should be empty")</script>';
+        echo '<script>alert("All fields are mandatory")</script>';
     }
     else{
 
@@ -38,7 +38,7 @@ if(isset($_POST['dbt_crd_submit'])){
     $result = $conn->query($sql);
     if($result->num_rows <= 0){
 
-        echo '<script>alert("No Data match with the details provided")</script>';
+        echo '<script>alert("Details mismatch")</script>';
 
     }
     
@@ -47,7 +47,7 @@ if(isset($_POST['dbt_crd_submit'])){
     $row = $result->fetch_assoc();
     if(!is_numeric($mob) || (strlen($mob) > 10 || strlen($mob) < 10)){
 
-        echo '<script>alert("Invalid Mobile Number\nPlease enter 10 Digit registered mobile number")</script>';
+        echo '<script>alert("Mobile Number Invalid\n Please enter 10 Digit registered mobile number")</script>';
 
         }
 
@@ -57,25 +57,23 @@ if(isset($_POST['dbt_crd_submit'])){
         }
         elseif($holder_name != $row['Username']){
 
-            echo '<script>alert("Incorrect Account Holder Name")</script>';
+            echo '<script>alert("User name not found in our records")</script>';
             echo $row['Username'];
         }
         elseif($dob != $row['DOB']){
 
-            echo '<script>alert("Incorrect Date of Birth\nPlease enter Date of Birth as on PAN Card")</script>';
+            echo '<script>alert("Incorrect Date of Birth")</script>';
     
         }
-        elseif($PAN != $row['pan']){
+        elseif($SSN != $row['SSN']){
 
-            echo '<script>alert("Incorrect PAN Number")</script>';
+            echo '<script>alert("SSN number not found")</script>';
 
         }
      
 
         else{
-            //-------------------------------------------------------------------'
-
-            //Code to Issue Debit Card since all the provided details are correct
+            
             
             $mob_no = $row['Mobile_no'];
            if($row['Debit_Card_No'] === NULL){
@@ -86,32 +84,10 @@ if(isset($_POST['dbt_crd_submit'])){
             if($conn->query($sql) == TRUE ){
 
 
-                //SMS Integration for Debit Card Details  -----------------------------------------------------
-						
-					// require('textlocal.class.php');
-					// $apikey = 'Mzie479SxfY-Z7slYf9AI3zVXCAu0G5skUBQVYOfRU';
-					// $textlocal = new Textlocal(false,false,$apikey);
-					// $numbers = array($mob_no);
-					// $sender = 'TXTLCL';
-					// $message = 'Hello '.$row['Username'].' Your Debit Card No is : '.$debit_card.' with the auto generated pin : '.$debit_card_pin.' Please change this pin as soon as possible';
-
-					
-					// 	try {
-					// 		$result = $textlocal->sendSms($numbers, $message, $sender);
-					// 		print_r($result);
-					// 	} catch (Exception $e) {
-					// 		die('Error: ' . $e->getMessage());
-					// 	}
-						
-		//--------------------------------------------------------------------------------------				
-		//--------------------------------------------------------------------------------------
-	
-
-
-            echo '<script>alert("Debit Card issued successfully.\n\nIt will be delivered to your home address soon.\n\nYour Debit Card No is : '.$debit_card.' and Pin is : '.$debit_card_pin.'\n\n Please change this pin as soon as possible.")</script>';
+                
+            echo '<script>alert("Your Debit Card has been successfully issued and will be delivered to your home address shortly\n\n. Your Debit Card No is '.$debit_card.' and your Pin is '.$debit_card_pin.'.\n\n For security reasons, we recommend changing your Pin as soon as possible.")</script>';
                 
             }
-            //--------------------------------------------------------------------
         }
 
         else{
